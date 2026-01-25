4b2abe18b64bd55c3444a6cdfa55e1a46cf5e2f1
homemac <homemac>
2026-01-25 13:23:40 +0900

    Update speed

--- app/[username]/layout.tsx
+++ app/[username]/layout.tsx
@@ -1,14 +1,22 @@
 //app/[username]/layout.tsx
 import { headers } from "next/headers";
 import AppKitProvider from "@/context/AppKitProvider";
+import BottomNav from "@/components/BottomNav";
 
-export default async function UsernameLayout({
-  children,
-}: {
+type Props = {
   children: React.ReactNode;
-}) {
+  params: Promise<{ username: string }>;
+};
+
+export default async function UsernameLayout({ children, params }: Props) {
+  const { username } = await params;
   const headerObj = await headers();
   const cookies = headerObj.get("cookie");
 
-  return <AppKitProvider cookies={cookies}>{children}</AppKitProvider>;
+  return (
+    <AppKitProvider cookies={cookies}>
+      <div className="min-h-screen pb-24">{children}</div>
+      <BottomNav username={username} />
+    </AppKitProvider>
+  );
 }
