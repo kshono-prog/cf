63f233c6773bc0a25f5c6788a4a0b8f162bf3f9a
homemac <homemac>
2026-03-16 13:47:06 +0900

    Update AIAgent

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -32,15 +32,13 @@ export function AppHeader({ username }: { username: string }) {
       <div className="mx-auto flex h-[60px] w-full max-w-[760px] items-center justify-between px-3 sm:px-6">
         <Link href={`/${username}/home`} className="shrink-0">
           <div className="flex items-center">
-            <div className="flex h-9 w-9 items-center justify-center rounded-[14px] border border-[var(--line)] bg-[var(--surface)] shadow-sm">
-              <Image
-                src="/icon/icon-cf.png"
-                alt="Creator Founding"
-                width={24}
-                height={24}
-                className="h-6 w-6 rounded-[8px]"
-              />
-            </div>
+            <Image
+              src="/icon/icon-cf.png"
+              alt="Creator Founding"
+              width={36}
+              height={36}
+              className="h-9 w-9 rounded-[8px]"
+            />
           </div>
         </Link>
 
